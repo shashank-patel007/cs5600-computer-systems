f6258f31afed3acb40bd8d394e397d3d36ec1ea4
shashank-patel007 <shashankcr777@gmail.com>
2025-02-03 16:42:15 -0500

    added initial setup for project1

---
+++ project1/hello.c
@@ -0,0 +1,12 @@
+/*
+ * file: hello.c - project1-part-2 micro-program
+ */
+
+extern void print(char *buf);
+
+int main(void)
+{
+    print("Hello world!\n");
+    return 0;
+}
+
---
+++ project1/wait.c
@@ -0,0 +1,13 @@
+/*
+ * file: wait.c - project-1-part-1 microprogram
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j;
+	for (i = 0; i < 10000; i++)
+		for (j = 0; j < 100000; j++)
+			;
+        return 0;
+}
+
